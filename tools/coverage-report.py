99a5c1827374c6187909a9b7da7576ceb9654185
Alexey Kayunov <akayunov@intermedia.net>
2017-08-24 12:25:48 +0300

    #1 add test

--- tools/coverage-report.py
+++ tools/coverage-report.py
@@ -6,9 +6,12 @@ import os.path
 # --cov-report annotate
 # --cov=myproj tests/
 scr_dir = os.path.dirname(os.path.realpath(__file__))
+tmp_dir = os.path.join(os.path.dirname(scr_dir), 'tmp')
 subprocess.call([
     os.path.join(scr_dir, '../../venv/bin/py.test'),
     '--cov', os.path.join(scr_dir, '../lib'),
-    '--cov-report', 'annotate',
+    '--cov-report', 'xml:' + tmp_dir + '/cov_xml',
+    '--cov-report', 'html:' + tmp_dir + '/cov_html',
+    '--cov-report', 'annotate:' + tmp_dir + '/cov_annotate',
     os.path.join(scr_dir, '../test')]
 )
